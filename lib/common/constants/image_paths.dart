8b06451c2c58e06ede6c05314b2037a0215fa02e
Shihab Kandil <jupitar.orbit01@gmail.com>
2023-05-13 00:27:52 +0300

    Add Fonts

--- lib/common/constants/image_paths.dart
+++ lib/common/constants/image_paths.dart
@@ -1,5 +1,6 @@
-class ImagePath{
+class ImagePaths{
   static const String logo = 'assets/images/Logo.png';
+  static const String lightSplashPattern = 'assets/images/light_splash_pattern.png';
   static const String firstOnBoarding = 'assets/images/illustrations/onBoarding1.png';
   static const String secondOnBoarding = 'assets/images/illustrations/onBoarding2.png';
 }
