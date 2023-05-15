488d4b1a23c84affb5e15a2434e0034a38cbb62f
Shihab Kandil <jupitar.orbit01@gmail.com>
2023-05-15 22:34:27 +0300

    Add Svg assets

---
+++ lib/common/constants/assets_paths.dart
@@ -0,0 +1,13 @@
+class AssetsPaths {
+  static const String logo = 'assets/images/Logo.png';
+  static const String lightSplashPattern =
+      'assets/images/light_splash_pattern.png';
+  static const String firstOnBoarding =
+      'assets/images/illustrations/onBoarding1.png';
+  static const String secondOnBoarding =
+      'assets/images/illustrations/onBoarding2.png';
+
+  static const String messageSvgIcon = 'assets/icons/Message.svg';
+
+  static const String lockSvgIcon = 'assets/icons/Lock.svg';
+}
